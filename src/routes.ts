0a556da94f564054cd66e2ba148eb26ab67374e4
naisamelo <naisamelo@gmail.com>
2023-02-22 16:55:45 -0300

    Verificando o registro do hábito

--- src/routes.ts
+++ src/routes.ts
@@ -102,17 +102,30 @@ export async function appRoutes(app: FastifyInstance ) {
                 }
             })
         }
-        //completar o hábito nesse dia
-        await prisma.dayHabit.create({
-            data: {
-                day_id: day.id,
-                habit_id: id,
+
+        const dayHabit = await prisma.dayHabit.findUnique({
+            where: {
+                day_id_habit_id: {
+                    day_id: day.id,
+                    habit_id: id,
+                }
             }
         })
+
+        if (dayHabit){
+            //remover a marcação de completo
+        } else {
+             //completar o hábito nesse dia
+            await prisma.dayHabit.create({
+                data: {
+                    day_id: day.id,
+                    habit_id: id,
+                }
+             })
+        }
+       
     })
-
     
-
 }
 
 
