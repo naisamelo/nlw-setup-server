62e27bca8f7159d355c15a30494e2f0f02bd9562
naisamelo <naisamelo@gmail.com>
2023-02-25 20:27:06 -0300

    Finalização do banco de dados

--- src/routes.ts
+++ src/routes.ts
@@ -114,6 +114,11 @@ export async function appRoutes(app: FastifyInstance ) {
 
         if (dayHabit){
             //remover a marcação de completo
+            await prisma.dayHabit.delete({
+                where: {
+                    id: dayHabit.id,
+                }
+            })
         } else {
              //completar o hábito nesse dia
             await prisma.dayHabit.create({
@@ -125,7 +130,34 @@ export async function appRoutes(app: FastifyInstance ) {
         }
        
     })
-    
+
+    app.get('/summary', async () =>{
+        // Quey mais complexa, mais condições, relacionamentos => SQL na mão (RAW)
+        // Prisma ORM: RAW SQL => SQLite
+
+        const summary = await prisma.$queryRaw`
+         SELECT
+           D.id,
+           D.date,
+           (
+             SELECT
+               cast(count(*) as float)
+             FROM day_habits DH
+             WHERE DH day_id = D.id
+           ) as completed,
+           (
+             SELECT
+                cast(count(*) as float)
+             FROM habit_week_days HWD
+             JOIN habiTS H
+                ON H.id = HWD.habit_id
+             WHERE
+                HWD.week_day = cast(stftime('%w', D.date/1000.0, 'unixepoch') as int)
+                AND H.created_at <= D.date
+           ) as amount
+         FROM days D
+        `
+    })
 }
 
 
